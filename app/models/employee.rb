c6da280df3c3f9b3abf8849d31ec03db355a75a0
Artur Pedrosa <maverickanp@gmail.com>
2023-07-28 11:52:07 -0300

    ajuste na logica de validacao de ferias do colaborador

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,4 +2,9 @@ class Employee < ApplicationRecord
 has_many :vacations
 validates :name, :position, :hire_date, presence: true
 validates :hire_date, timeliness: { on_or_before: lambda { Date.current }, type: :date }
+
+    def vacation_days_available
+        months_since_hire = (Date.today.year * 12 + Date.today.month) - (self.hire_date.year * 12 + self.hire_date.month)
+        (30 * (months_since_hire / 12.0)).to_i
+    end
 end
--- app/models/vacation.rb
+++ app/models/vacation.rb
@@ -2,17 +2,43 @@ class Vacation < ApplicationRecord
   belongs_to :employee
 
   validates :start_date, :end_date, presence: true
-  validate :start_date_no_overlap, :min_duration
+  validate :validate_vacation_days
+  validate :validate_vacation_periods
+  validate :validate_consecutive_days
 
-  def start_date_no_overlap
-    if employee.vacations.where.not(id: id).any? { |vacations| (start_date..end_date).overlaps?(vacations.start_date..vacations.end_date) }
-      errors.add(:start_date, "já está agendada para outro período de férias")
+  private
+
+  def validate_vacation_days
+    vacation_days = (self.end_date - self.start_date).to_i + 1
+    employee_vacation_days_available = self.employee.vacation_days_available
+
+    if vacation_days < 10 || vacation_days > employee_vacation_days_available
+      errors.add(:base, "Nao pode ter menos que 10 dias consecutivos")
     end
   end
 
-  def min_duration
-    if (end_date - start_date).to_i < 10
-      errors.add(:end_date, "deve ser pelo menos 10 dias após a data de início")
+  def validate_vacation_periods
+    overlapping_vacations = Vacation.where(employee_id: self.employee_id)
+                                    .where.not(id: self.id)
+                                    .where('(start_date <= ? AND end_date >= ?) OR (start_date >= ? AND start_date <= ?)',
+                                           self.start_date, self.start_date, self.start_date, self.end_date)
+                                    .count
+
+    if overlapping_vacations > 0
+      errors.add(:base, "O periodo de ferias do colaborador nao pode ser sobreposto")
     end
   end
+
+  def validate_consecutive_days
+    vacation_days = (self.end_date - self.start_date).to_i + 1
+
+    if vacation_days < 14
+      errors.add(:base, "Voce precisa agendar ao menos um periodo maior que 14 dias consecutivos")
+    end
+
+    if vacation_days < 5
+      errors.add(:base, "Nao pode ter menos que 5 dias consecutivos")
+    end
+  end
+
 end
